6a06f96b3659bcce27bfbb623c785a6d4212def1
unknown <huenk105@gmail.com>
2022-04-13 23:02:55 +0700

    demo

---
+++ src/com/codegym/AbstractTree.java
@@ -0,0 +1,9 @@
+package com.codegym;
+
+public abstract class AbstractTree<E> implements Tree<E>{
+    @Override
+    public void inorder(){
+
+    }
+
+}
---
+++ src/com/codegym/TestBST.java
@@ -0,0 +1,17 @@
+package com.codegym;
+
+public class TestBST {
+    public static void main(String[] args) {
+        BST<String> tree = new BST<>();
+        tree.insert("George");
+        tree.insert("Michael");
+        tree.insert("Tom");
+        tree.insert("Adam");
+        tree.insert("Jones");
+        tree.insert("Peter");
+        tree.insert("Daniel");
+        System.out.println("Inoder (sorted): ");
+        tree.inorder();
+        System.out.println("The number of nodes is: "+tree.getSize());
+    }
+}
